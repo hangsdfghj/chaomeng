e9305e0469756ee0ef7d5baf31045b79e09bf547
hangsdfghj <hangsdfghj@gmail.com>
2026-06-12 03:02:07 +0800

    更新3:02

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3786950031"
+    serviceWorkerVersion: "2809557988"
   }
 });
