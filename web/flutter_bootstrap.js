4d32c4653d4238ae4de8c81408c18b15b96cfd14
hangsdfghj <hangsdfghj@gmail.com>
2026-06-17 23:17:24 +0800

    1117

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2476468218"
+    serviceWorkerVersion: "2324508247"
   }
 });
