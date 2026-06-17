1ddcc2d8f43ed1ff13d6eb94fe66cd0865b0ad40
hangsdfghj <hangsdfghj@gmail.com>
2026-06-17 22:33:10 +0800

    0617
    鍵盤調整

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2434878588"
+    serviceWorkerVersion: "1939349571"
   }
 });
